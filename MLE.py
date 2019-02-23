9ad5111ee2f187ea81ae3eea7e338989fcaa833f
Chengzhi Mao <mcz13@mails.tsinghua.edu.cn>
2019-02-23 17:03:14 -0500

    correct version

--- MLE.py
+++ MLE.py
@@ -3,25 +3,30 @@ import numpy as np
 
 
 
-def dataset_process(filename):
+def dataset_process(filename, is_train, mean):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         cnt =0
         row_str_list=[]
-        for row in csv_file:
-            if len(row)>1 and len(row)<40:
+        for i, row in enumerate(csv_reader):
+            if len(row)>1 and i>0:
                 row_str_list.append(row)
                 cnt += 1
 
         y_gt = np.zeros((cnt, 1))
         features = np.zeros((cnt, 9))
 
-        for i, row in enumerate(row_str_list):
-            row_list = row.split(',')
-            y_gt[i] = row_list[0]
-
+        for i, row_list in enumerate(row_str_list):
+            y_gt[i] = int(row_list[0])
             for j in range(1, 10):
                 features[i, j-1] = row_list[j]
+
+        if is_train:
+            # print("shape", features.shape, np.mean(features, axis=0).shape)
+            mean = np.mean(features, axis=0)
+            features = features - np.mean(features, axis=0)
+        else:
+            features = features - mean
 
         type_0 = np.zeros((cnt - int(np.sum(y_gt)), 9))
         type_1 = np.zeros((int(np.sum(y_gt)), 9))
@@ -35,7 +40,7 @@ def dataset_process(filename):
             elif y_gt[i] == 1:
                 type_1[c1] = features[i]
                 c1 += 1
-    return y_gt, features, type_0, type_1
+    return y_gt, features, type_0, type_1, mean
 
 
 
@@ -45,22 +50,33 @@ def dataset_process(filename):
 if __name__ == '__main__':
     trainfilename = 'propublicaTrain.csv'
     testfilename = 'propublicaTrain.csv'
-    y_gt, features, type_0, type_1 = dataset_process(trainfilename)
+    y_gt, features, type_0, type_1, mean = dataset_process(trainfilename, True, [])
 
-    test_y_gt, test_features, test_type_0, test_type_1 = dataset_process(testfilename)
+    test_y_gt, test_features, test_type_0, test_type_1,_ = dataset_process(testfilename, False, mean)
+
+    print('type0', type_0)
+    print('type1', type_1)
 
     mean0 = np.mean(type_0, axis=0)
     mean1 = np.mean(type_1, axis=0)
 
-    A0 = type_0 - mean0
-    A1 = type_1 - mean1
+    print('mean0', mean0, 'mean1', mean1)
 
+    A0 = type_0 - mean0 # [len, 9]
+    A1 = type_1 - mean1 # [l2, 9]
+
+    print("****A0")
     print(A0.shape)
+    print("****A1")
     print(A1)
+    print("****")
 
+    sigma0 = np.dot(np.transpose(A0), A0) / A0.shape[0]
+    sigma1 = np.dot(np.transpose(A1), A1) / A1.shape[0]
 
-    sigma0 = np.dot(np.transpose(A0), A0)
-    sigma1 = np.dot(np.transpose(A1), A1)
+    # print(sigma0)
+    print("***sigma1*")
+    print(sigma1)
 
     print(np.linalg.det(sigma0))
     print(np.linalg.det(sigma1))
@@ -70,8 +86,6 @@ if __name__ == '__main__':
 
     inv_simgma0 = np.linalg.inv(sigma0)
     inv_simgma1 = np.linalg.inv(sigma1)
-
-    print(mean0, mean1)
 
     # for class 0
 
